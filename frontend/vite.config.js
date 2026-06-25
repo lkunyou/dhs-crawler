8cdf3c320885107b80121ac2d7da0ad831467689
lkunyou <lkunyou@126.com>
2026-06-25 16:16:53 +0800

    0.1环境

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
-    }
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  // 根据当前mode加载对应.env文件变量
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+  const apiTarget = env.VITE_API_TARGET
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src')
+      }
+    },
+    server: {
+      port: 5173,
+      proxy: {
+        '/dhs-crawler/api': {
+          target: apiTarget,
+          changeOrigin: true,
+        }
       }
     }
   }
